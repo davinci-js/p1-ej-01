8e44fe381837c4fe14b069370acdd9d9ece4f30c
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-07 20:53:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function click() {
   /* Obtiene el limite inferior */
   let inf = parseFloat(document.getElementById("inf-limit").value);
   /* Obtiene el limite inferior */
-  let inf = parseFloat(document.getElementById("sup-limit").value);
+  let sup = parseFloat(document.getElementById("sup-limit").value);
   /* Obtiene el numero del cuadro de entrada */
   let numero = parseFloat(document.getElementById("input").value);
   

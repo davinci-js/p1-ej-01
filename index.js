051779ab5f31adb1383adefd1e42158249ec40bb
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-08 22:36:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 "use strict";
 
-function clicked() {
-
-  /* Obtiene el limite inferior */
-  let inf = parseFloat(document.getElementById("inf-limit").value);
-  /* Obtiene el limite inferior */
-  let sup = parseFloat(document.getElementById("sup-limit").value);
-  /* Obtiene el numero del cuadro de entrada */
-  let numero = parseFloat(document.getElementById("input").value);
-  
-  /* TODO */
-
-}
+/* TODO */
